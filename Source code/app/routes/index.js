75908586f4507c8332d33c908ab589014570071b
Tran Tuan Lam <53012411+lamtuantran1997@users.noreply.github.com>
2021-07-11 12:39:41 +0700

    Add files via upload

--- Source code/app/routes/index.js
+++ Source code/app/routes/index.js
@@ -5,7 +5,7 @@ const middlewares = require('../middlewares');
 module.exports = (app) => {
     require('./user_routes')(router, controller, middlewares);
     require('./form_routes')(router,controller,middlewares);
-    require('./report_routes')(router,controller);
-    app.use('/report',middlewares.checkRole.isHr);
+    require('./report_routes')(router,controller,middlewares);
     app.use('/hrms', router);
+    
 }
--- Source code/app/routes/report_routes.js
+++ Source code/app/routes/report_routes.js
@@ -1,4 +1,5 @@
-module.exports = (router, controller) => {
+module.exports = (router, controller,middleware) => {
+    router.use('/user/report',middleware.checkRole.isHr);
     router.get('/user/report/notFinishedAssessmentForm',controller.report.usersNotFinishedForm);
     router.get('/user/report/notFinishedProbationaryForm', controller.report.usersNotFinishedForm);
     router.get('/user/report/finishedAssessmentForm', controller.report.usersFinishedForm);
